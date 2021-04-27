edfee3824f1b37df6969feeb69581d69b8289fe8
Joaquin Lopez <mrgus@disco-zombie.net>
2021-04-27 10:17:30 -0700

    Manifest and lambda base calico configs, man deflt

--- cdk/eks/eks_stack.py
+++ cdk/eks/eks_stack.py
@@ -14,6 +14,9 @@ import aws_cdk.aws_ec2 as ec2
 import aws_cdk.aws_efs as efs
 import aws_cdk.aws_eks as eks
 import aws_cdk.aws_iam as iam
+import aws_cdk.aws_lambda as aws_lambda
+from aws_cdk.lambda_layer_kubectl import KubectlLayer
+from aws_cdk.lambda_layer_awscli import AwsCliLayer
 
 
 manifests = [["calico", "https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/v1.7.10/config/master/calico.yaml"]]
@@ -29,7 +32,7 @@ class EksStack(cdk.Stack):
         self.name = self.config["name"]
         self.buckets = {}
 
-        s3_api_statement = s3_bucket_statement = iam.PolicyStatement(
+        self.s3_api_statement = s3_bucket_statement = iam.PolicyStatement(
             actions=[
                 "s3:PutObject",
                 "s3:GetObject",
@@ -186,25 +189,7 @@ class EksStack(cdk.Stack):
                 asg_policy.attach_to_role(ng.role)
                 c += 1
 
-        # This produces an obnoxious diff on every subsequent run
-        # Using a helm chart does not, so we should switch to that
-        # However, we need to figure out how to get the helm chart
-        # accessible by the CDK lambda first. Not clear how to give
-        # s3 perms to it programmatically, and while ECR might be
-        # an option it also doesn't seem like there's a way to push
-        # the chart with existing api calls.
-        # Probably need to do some custom lambda thing.
-        for manifest in manifests:
-            filename = f"{manifest[0]}.yaml"
-            if isfile(filename):
-                with open(filename) as f:
-                    manifest_text = f.read()
-            else:
-                manifest_text = requests_get(manifest[1]).text
-            loaded_manifests = [yaml_safe_load(i) for i in re_split("^---$", manifest_text, flags=MULTILINE) if i]
-            crds = eks.KubernetesManifest(self, "calico-crds", cluster=self.cluster, manifest=[crd for crd in loaded_manifests if crd["kind"] == "CustomResourceDefinition"])
-            non_crds = eks.KubernetesManifest(self, "calico", cluster=self.cluster, manifest=[notcrd for notcrd in loaded_manifests if notcrd["kind"] != "CustomResourceDefinition"])
-            non_crds.node.add_dependency(crds)
+        self.install_calico()
 
         self.efs = efs.FileSystem(
             self,
@@ -239,6 +224,73 @@ class EksStack(cdk.Stack):
                 # secondary_gids
             ),
         )
+
+    def install_calico(self):
+        self._install_calico_manifest()
+
+    def _install_calico_manifest(self):
+        # This produces an obnoxious diff on every subsequent run
+        # Using a helm chart does not, so we should switch to that
+        # However, we need to figure out how to get the helm chart
+        # accessible by the CDK lambda first. Not clear how to give
+        # s3 perms to it programmatically, and while ECR might be
+        # an option it also doesn't seem like there's a way to push
+        # the chart with existing api calls.
+        # Probably need to do some custom lambda thing.
+        for manifest in manifests:
+            filename = f"{manifest[0]}.yaml"
+            if isfile(filename):
+                with open(filename) as f:
+                    manifest_text = f.read()
+            else:
+                manifest_text = requests_get(manifest[1]).text
+            loaded_manifests = [yaml_safe_load(i) for i in re_split("^---$", manifest_text, flags=MULTILINE) if i]
+            crds = eks.KubernetesManifest(self, "calico-crds", cluster=self.cluster, manifest=[crd for crd in loaded_manifests if crd["kind"] == "CustomResourceDefinition"])
+            non_crds = eks.KubernetesManifest(self, "calico", cluster=self.cluster, manifest=[notcrd for notcrd in loaded_manifests if notcrd["kind"] != "CustomResourceDefinition"])
+            non_crds.node.add_dependency(crds)
+
+    def _install_calico_lambda(self):
+        # WIP
+        k8s_lambda = aws_lambda.Function(
+            self,
+            "k8s_lambda",
+            handler="main",
+            runtime=aws_lambda.Runtime.PROVIDED,
+            layers=[
+                KubectlLayer(self, "KubectlLayer"),
+                AwsCliLayer(self, "AwsCliLayer"),
+            ],
+            vpc=self.vpc,
+            code=aws_lambda.AssetCode("cni-bundle.zip"),
+            timeout=core.Duration.seconds(30),
+            environment={
+                "cluster_name": self.cluster.cluster_name
+            },
+            security_groups=self.cluster.connections.security_groups,
+        )
+        self.cluster.connections.allow_default_port_from(self.cluster.connections)
+        k8s_lambda.add_to_role_policy(self.s3_api_statement)
+        #k8s_lambda.add_to_role_policy(iam.PolicyStatement(
+        #    actions=["eks:*"],
+        #    resources=[self.cluster.cluster_arn],
+        #))
+        self.cluster.aws_auth.add_masters_role(k8s_lambda.role)
+        #run_calico_lambda.node.add_dependency(k8s_lambda)
+
+        # This got stuck, need to make a response object?
+        #run_calico_lambda = core.CustomResource(
+        #    self,
+        #    "run_calico_lambda",
+        #    service_token=k8s_lambda.function_arn,
+        #)
+        from aws_cdk.aws_stepfunctions_tasks import LambdaInvoke
+        LambdaInvoke(
+            self,
+            "run_calico_lambda",
+            lambda_function=k8s_lambda,
+            timeout=core.Duration.seconds(120),
+        )
+
 
     # Override default max of 2 AZs, as well as allow configurability
     @property
