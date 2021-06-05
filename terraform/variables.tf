1140affed4a7a8f07fdbe321e203afbce122e678
Steven Davidovitz <steved@users.noreply.github.com>
2021-06-04 18:07:07 -0700

    update cloudformation stack terraform lifecycle timeouts  (#20)
    * update cloudformation stack terraform lifecycle timeouts
    
    Matches intention in #18
    
    * terraform fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,46 +1,46 @@
 variable "asset_bucket" {
-    type        = string
-    description = "Bucket to create and deploy CDK assets to"
+  type        = string
+  description = "Bucket to create and deploy CDK assets to"
 }
 
 variable "asset_dir" {
-    type        = string
-    description = "Local path of CDK asset directory"
+  type        = string
+  description = "Local path of CDK asset directory"
 }
 
 variable "aws_region" {
-    type        = string
-    description = "AWS Region to deploy stack into"
+  type        = string
+  description = "AWS Region to deploy stack into"
 }
 
 variable "name" {
-    type        = string
-    description = "Unique identifier for deployment"
+  type        = string
+  description = "Unique identifier for deployment"
 }
 
 variable "output_dir" {
-    type        = string
-    description = "Output directory for agent_template.yaml and kubeconfig"
+  type        = string
+  description = "Output directory for agent_template.yaml and kubeconfig"
 }
 
 variable "parameters" {
-    type        = map
-    description = "Parameters to feed into cloudformation"
+  type        = map
+  description = "Parameters to feed into cloudformation"
 }
 
 variable "template_filename" {
-    type        = string
-    description = "Filename of CloudFormation Template file in asset_dir (usually <stack_name>.template.json)"
+  type        = string
+  description = "Filename of CloudFormation Template file in asset_dir (usually <stack_name>.template.json)"
 }
 
 variable "iam_role_arn" {
-    type        = string
-    description = "IAM role to use for deployment"
-    default     = ""
+  type        = string
+  description = "IAM role to use for deployment"
+  default     = ""
 }
 
 variable "cloudformation_timeout_in_minutes" {
-    type        = number
-    description = "CloudFormation provisioning timeout in minutes"
-    default     = 60
+  type        = number
+  description = "CloudFormation provisioning timeout in minutes"
+  default     = 60
 }
