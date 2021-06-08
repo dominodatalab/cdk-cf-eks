ca659c9a27e2266eb4754014e848615b3d68bb40
Joaquin Lopez <mrgus@disco-zombie.net>
2021-06-08 13:01:26 -0700

    Add semantic_version req

--- cdk/setup.py
+++ cdk/setup.py
@@ -36,6 +36,7 @@ setuptools.setup(
         "PyYAML~=5.4.1",
         "isort~=5.8.0",
         "flake8~=3.9.1",
+        "semantic_version~=2.8.5",
     ],
     python_requires=">=3.6",
     classifiers=[
