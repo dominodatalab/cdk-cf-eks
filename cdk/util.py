f183a50f4d4e688949eced532cac870b289b6778
Joaquin Lopez <mrgus@disco-zombie.net>
2021-06-15 22:46:32 -0700

    Config file loader/dumper
    Can be used either as a linter or as a schema upgrader

--- cdk/util.py
+++ cdk/util.py
@@ -3,8 +3,10 @@
 import argparse
 from sys import stdout
 
-from ruamel.yaml import YAML
+from ruamel.yaml import YAML, SafeLoader
+from ruamel.yaml import load as yaml_load
 
+from domino_cdk.config import config_loader
 from domino_cdk.config.template import config_template
 
 def parse_args():
@@ -22,6 +24,12 @@ def parse_args():
     template_parser.add_argument("--gpu-nodegroups", help="How many compute nodegroups per az", default=1, type=int)
     template_parser.set_defaults(func=generate_config_template)
 
+    load_parser = subparsers.add_parser("load_config", help="Load config into memory for linting/updating")
+    load_parser.add_argument("-f", "--file", help="File to load, otherwise reads stdin", default=None)
+    load_parser.add_argument("-o", "--out-file", help="File to write to or '-' for stdout [default: none]", default=None)
+    load_parser.add_argument("--no-comments", help="Strip comments from template", action="store_true")
+    load_parser.set_defaults(func=load_config)
+
     return parser.parse_args()
 
 def generate_config_template(args):
@@ -34,6 +42,17 @@ def generate_config_template(args):
         private_api=args.private_api,
         dev_defaults=args.dev,
     ).render(args.no_comments), stdout)
+
+def load_config(args):
+    print(f"Loading config {args.file or 'from stdin'}...")
+    with open(args.file or 0) as f:
+        cfg = config_loader(yaml_load(f, Loader=SafeLoader))
+
+    print("Config loaded successfully")
+
+    if args.out_file:
+        with open(1 if args.out_file == "-" else args.out_file, "w") as out:
+            YAML().dump(cfg.render(args.no_comments), out)
 
 if __name__ == "__main__":
     args = parse_args()
