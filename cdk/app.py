8e0e628b37c48c52b1f25437558f909279439df0
Joaquin Lopez <mrgus@disco-zombie.net>
2021-06-17 19:45:47 -0700

    Move cli commands into util.py

--- cdk/app.py
+++ cdk/app.py
@@ -1,58 +1,34 @@
 #!/usr/bin/env python3
-from json import dumps as json_dumps
-from sys import argv, stdout
-
 from aws_cdk import core
-from ruamel.yaml import YAML, SafeLoader
+from ruamel.yaml import SafeLoader
 from ruamel.yaml import load as yaml_load
 
 from domino_cdk.config import config_loader
-from domino_cdk.config.template import config_template
 from domino_cdk.domino_stack import DominoStack
-from domino_cdk.util import DominoCdkUtil
 
+app = core.App()
 
-def main():
-    app = core.App()
+with open(app.node.try_get_context("config") or "config.yaml") as f:
+    cfg = config_loader(yaml_load(f, Loader=SafeLoader))
 
-    with open(app.node.try_get_context("config") or "config.yaml") as f:
-        cfg = config_loader(yaml_load(f, Loader=SafeLoader))
+nest = app.node.try_get_context("nest") or False
 
-    nest = app.node.try_get_context("nest") or False
+DominoStack(
+    app,
+    f"{cfg.name}-domino",
+    # If you don't specify 'env', this stack will be environment-agnostic.
+    # Account/Region-dependent features and context lookups will not work,
+    # but a single synthesized template can be deployed anywhere.
+    # Uncomment the next line to specialize this stack for the AWS Account
+    # and Region that are implied by the current CLI configuration.
+    # env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    # Uncomment the next line if you know exactly what Account and Region you
+    # want to deploy the stack to. */
+    # env=core.Environment(account='123456789012', region='us-east-1'),
+    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+    env=core.Environment(region=cfg.aws_region, account=cfg.aws_account_id),
+    cfg=cfg,
+    nest=nest,
+)
 
-    DominoStack(
-        app,
-        f"{cfg.name}-eks-stack",
-        # If you don't specify 'env', this stack will be environment-agnostic.
-        # Account/Region-dependent features and context lookups will not work,
-        # but a single synthesized template can be deployed anywhere.
-        # Uncomment the next line to specialize this stack for the AWS Account
-        # and Region that are implied by the current CLI configuration.
-        # env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-        # Uncomment the next line if you know exactly what Account and Region you
-        # want to deploy the stack to. */
-        # env=core.Environment(account='123456789012', region='us-east-1'),
-        # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-        env=core.Environment(region=cfg.aws_region, account=cfg.aws_account_id),
-        cfg=cfg,
-        nest=nest,
-    )
-
-    app.synth()
-
-
-if __name__ == "__main__":
-    if len(argv) > 1:
-        if argv[1] == "generate_asset_parameters":
-            print(json_dumps(DominoCdkUtil.generate_asset_parameters(*argv[2:]), indent=4))
-        elif argv[1] == "generate_terraform_bootstrap":
-            print(json_dumps(DominoCdkUtil.generate_terraform_bootstrap(*argv[2:]), indent=4))
-        elif argv[1] == "generate_config_template":
-            YAML().dump(config_template().render(*argv[2:]), stdout)  # an arg disable comments
-        else:
-            print(
-                "Valid utility commands are 'generate_asset_parameters', 'generate_terraform_bootstrap' and 'generate_config_template'. Otherwise, use cdk."
-            )
-            exit(1)
-        exit(0)
-    main()
+app.synth()
