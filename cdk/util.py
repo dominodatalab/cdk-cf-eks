412aa5723b0b6f8d71ee67f8aa99f302172be7aa
Joaquin Lopez <mrgus@disco-zombie.net>
2021-06-17 19:45:47 -0700

    Move cli commands into util.py

--- cdk/util.py
+++ cdk/util.py
@@ -3,11 +3,15 @@
 import argparse
 from sys import stdout
 
+from json import dumps as json_dumps
 from ruamel.yaml import YAML, SafeLoader
 from ruamel.yaml import load as yaml_load
 
 from domino_cdk.config import config_loader
 from domino_cdk.config.template import config_template
+from domino_cdk.util import DominoCdkUtil
+
+DEFAULT_TF_MODULE_PATH = "https://github.com/dominodatalab/cdk-cf-eks/releases/download/v0.0.1rc1/domino-cdk-terraform-0.0.1rc1.tar.gz"
 
 def parse_args():
     parser = argparse.ArgumentParser(description="domino_cdk utility")
@@ -30,7 +34,23 @@ def parse_args():
     load_parser.add_argument("--no-comments", help="Strip comments from template", action="store_true")
     load_parser.set_defaults(func=load_config)
 
+    asset_parser = subparsers.add_parser("generate_asset_parameters", help="Generate CloudFormation parameters for CDK assets")
+    asset_parser.add_argument("-b", "--bucket", help="Name of bucket you plan to upload rendered CDK assets to (optional, default: __FILL__)", default="__FILL__")
+    asset_parser.add_argument("-d", "--dir", help="Directory with rendered CDK assets (optional, default: cdk.out)", default="cdk.out")
+    asset_parser.set_defaults(func=generate_asset_parameters)
+
+    tf_bootstrap_parser = subparsers.add_parser("generate_terraform_bootstrap", help="Generate Terraform bootstrap config")
+    tf_bootstrap_parser.add_argument("-m", "--module-path", help=f"Path to terraform module (optional, default: {DEFAULT_TF_MODULE_PATH})", default=DEFAULT_TF_MODULE_PATH)
+    tf_bootstrap_parser.add_argument("-b", "--bucket", help="Path to asset bucket, ie where to upload rendered CDK asset directory (optional, default: __FILL__)", default="__FILL__")
+    tf_bootstrap_parser.add_argument("-d", "--dir", help="Directory with rendered CDK assets (optional, default: cdk.out)", default="cdk.out")
+    tf_bootstrap_parser.add_argument("-r", "--aws-region", help="AWS Region")
+    tf_bootstrap_parser.add_argument("-o", "--output_dir", help="Directory Terraform will write kubeconfig/installer config to")
+    tf_bootstrap_parser.add_argument("--disable-random-templates", help="Disable randomly generated template names (used to trigger changes)", action="store_true", default=False)
+    tf_bootstrap_parser.add_argument("--iam-role-arn", help="IAM Role to assign to CloudFormation stack (optional, default: none)", default=None)
+    tf_bootstrap_parser.set_defaults(func=generate_terraform_bootstrap)
+
     return parser.parse_args()
+
 
 def generate_config_template(args):
     YAML().dump(config_template(
@@ -43,6 +63,7 @@ def generate_config_template(args):
         dev_defaults=args.dev,
     ).render(args.no_comments), stdout)
 
+
 def load_config(args):
     print(f"Loading config {args.file or 'from stdin'}...")
     with open(args.file or 0) as f:
@@ -53,6 +74,21 @@ def load_config(args):
     if args.out_file:
         with open(1 if args.out_file == "-" else args.out_file, "w") as out:
             YAML().dump(cfg.render(args.no_comments), out)
+
+
+def generate_asset_parameters(args):
+    print(json_dumps(
+        DominoCdkUtil.generate_asset_parameters(args.dir, args.bucket),
+        indent=4,
+    ))
+
+
+def generate_terraform_bootstrap(args):
+    print(json_dumps(
+        DominoCdkUtil.generate_terraform_bootstrap(args.module_path, args.bucket, args.dir, args.aws_region, args.output_dir, args.disable_random_templates, args.iam_role_arn),
+        indent=4,
+    ))
+
 
 if __name__ == "__main__":
     args = parse_args()
