59d596ed540c4f4f4b30abd3c5cd5b24b87b73ad
Michael Fraenkel <michael.fraenkel@dominodatalab.com>
2021-08-16 10:01:18 -0600

    PLAT-428: Remove helm version
    helm version is no longer supported since v3 is assumed.

--- cdk/domino_cdk/agent.py
+++ cdk/domino_cdk/agent.py
@@ -76,7 +76,6 @@ def generate_install_config(
         },
         "gpu": {"enabled": True},
         "helm": {
-            "version": 3,
             "cache_path": "charts",
         },
         "services": {
