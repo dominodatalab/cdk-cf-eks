9b95e34402d87ebd4fea7f68a025b3614152bf72
Joaquin Lopez <joaquin.lopez@dominodatalab.com>
2023-03-08 11:51:27 -0800

    Move iam out of main.tf, make order match deployer

--- convert/terraform/main.tf
+++ convert/terraform/main.tf
@@ -1,40 +1,20 @@
-data "aws_caller_identity" "admin" {}
-data "aws_partition" "current" {}
-
-resource "aws_iam_role" "grandfathered_creation_role" {
-  name = var.grandfathered_creation_role
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          AWS = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.admin.account_id}:root"
-        }
-      },
-    ]
-  })
-
-  lifecycle {
-    ignore_changes = [name, inline_policy]
-  }
-}
-
 module "domino_eks" {
   source                       = "github.com/dominodatalab/terraform-aws-eks.git?ref=v1.3.0"
   deploy_id                    = var.deploy_id
   region                       = var.region
-  default_node_groups          = var.default_node_groups
-  k8s_version                  = var.k8s_version
-  route53_hosted_zone_name     = var.route53_hosted_zone_name
-  eks_master_role_names        = var.eks_master_role_names
-  s3_force_destroy_on_deletion = var.s3_force_destroy_on_deletion
-  bastion                      = {}
-  ssh_pvt_key_path             = var.ssh_key_path
   tags                         = var.tags
+  k8s_version                  = var.k8s_version
+  default_node_groups          = var.default_node_groups
+  route53_hosted_zone_name     = var.route53_hosted_zone_name
+# cidr
+  bastion                      = {}
+  s3_force_destroy_on_deletion = var.s3_force_destroy_on_deletion
+  ssh_pvt_key_path             = var.ssh_key_path
+# kubeconfig_path
+# use_kms
+# kms_key_id
+# ecr_force_destroy_on_deletion
+  eks_master_role_names        = var.eks_master_role_names
   vpc_id                       = var.vpc_id
   public_subnets               = var.public_subnet_ids
   private_subnets              = var.private_subnet_ids
