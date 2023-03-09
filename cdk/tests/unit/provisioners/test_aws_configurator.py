60b8bddda7e74e512cc61294e343822971d1d953
Miguel Harmant <miguel.harmant@dominodatalab.com>
2023-03-09 10:26:06 -0500

    PLAT-6357 Update tests

--- cdk/tests/unit/provisioners/test_aws_configurator.py
+++ cdk/tests/unit/provisioners/test_aws_configurator.py
@@ -1,11 +1,6 @@
-from json import loads
-from os import chdir
-from tempfile import TemporaryDirectory
-
 import aws_cdk.aws_eks as eks
 from aws_cdk.assertions import Template
 from aws_cdk.core import App, Environment, Stack
-from ruamel.yaml import YAML
 
 from domino_cdk.aws_configurator import DominoAwsConfigurator
 
@@ -22,38 +17,4 @@ class TestDominoAwsConfigurator(TestCase):
         DominoAwsConfigurator(self.stack, self.eks_cluster)
 
         assertion = Template.from_stack(self.stack)
-        assertion.resource_count_is("Custom::AWSCDK-EKS-KubernetesResource", 21)  # 20 calico, one aws-auth
-
-        template = self.app.synth().get_stack("calico").template
-
-        crds_resource = next(res for name, res in template["Resources"].items() if name.startswith("calicocrds"))
-        self.assertTrue(len(loads(crds_resource["Properties"]["Manifest"])) > 0)
-
-        noncrds_resource = next(
-            res for name, res in template["Resources"].items() if name.startswith("calico") and res != crds_resource
-        )
-        self.assertTrue(len(loads(noncrds_resource["Properties"]["Manifest"])) > 0)
-
-    def test_install_calico_file(self):
-        with TemporaryDirectory() as tmpdir:
-            chdir(tmpdir)
-
-            with open("calico-operator.yaml", "w+") as f:
-                YAML().dump_all([{"kind": "DaemonSet", "apiVersion": "apps/v1", "metadata": {"name": "test-ds"}}], f)
-
-            DominoAwsConfigurator(self.stack, self.eks_cluster)
-
-        assertion = Template.from_stack(self.stack)
-        assertion.resource_count_is("Custom::AWSCDK-EKS-KubernetesResource", 2)  # two calico, one aws-auth
-
-        template = self.app.synth().get_stack("calico").template
-
-        crds_resource = next(
-            (res for name, res in template["Resources"].items() if name.startswith("calicocrds")), None
-        )
-        self.assertIsNone(crds_resource)
-
-        noncrds_resource = next(
-            res for name, res in template["Resources"].items() if name.startswith("calico") and res != crds_resource
-        )
-        self.assertEqual(len(loads(noncrds_resource["Properties"]["Manifest"])), 2)
+        assertion.resource_count_is("Custom::AWSCDK-EKS-HelmChart", 1)
